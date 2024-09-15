8f78749eeddb538402d33f37b57efe810340b69b
iamtobytom <tomtoby125@gmail.com>
2024-09-15 16:32:59 +0100

    login page

---
+++ app/Login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../components/Login'
+import Navbar from '../components/Navbar'
+import Navbar3 from '../components/Navbar3'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar3/>
+      <Login></Login>
+    </div>
+  )
+}
+
+export default page
